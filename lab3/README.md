6cf3c30f97fa7ed8e3f69b46e33e635d5fe17496
yoggys <61660055+yoggys@users.noreply.github.com>
2021-10-25 17:30:02 +0200

    finish lab3

--- lab3/README.md
+++ lab3/README.md
@@ -11,6 +11,7 @@ Required arguments:
 
 Optional arguments:
   -h, --help           show this help message and exit
+  --div DIV  Div class we want to scrap
   --out OUT  Name of out file
 ```
 
@@ -18,13 +19,20 @@ Optional arguments:
 Default values:
 
 out - 'out'
+div - None (print all page content)
 ```
 
 # Examples:
 
-```python lab3.py --url https://github.com/brokenpumpernickel/PwZN-2021Z-Live/blob/master/projects/project003.md```
+```python lab3.py --url https://github.com/brokenpumpernickel/PwZN-2021Z-Live/blob/master/projects/project003.md --div 'Box-body readme blob js-code-block-container p-5 p-xl-6 gist-border-0'```
  
 - Output .json file: 
 <p align="center">
   <img src="https://user-images.githubusercontent.com/61660055/138722425-e24fd5e0-dfd9-49b6-9002-1d6e92873c77.png" alt="Output .json file"/>
 </p>
+
+- Output .html file: 
+<p align="center">
+  <img src="https://user-images.githubusercontent.com/61660055/138722425-e24fd5e0-dfd9-49b6-9002-1d6e92873c77.png" alt="Output .html file"/>
+</p>
+
--- lab3/lab3.py
+++ lab3/lab3.py
@@ -10,6 +10,7 @@ def initParser():
     required = parser.add_argument_group('required arguments')
     optional = parser.add_argument_group('optional arguments')
     required.add_argument('--url', help="Url of website", type=str, required=True)
+    optional.add_argument('--div', help="Div class we want to scrap", type=str, default=None)
     optional.add_argument('--out', help="Name of out file", type=str, default='out')
 
     return parser
@@ -24,11 +25,21 @@ if __name__ == "__main__":
     except re.exceptions.HTTPError as E:
         print(E)
     except ConnectionError:
-        print("Website not found!")
+        print("Website not found...")
     else:
         if conn.status_code == 200:
             soup = BeautifulSoup(conn.text, "html.parser")
-            decoded = soup.decode(True, "utf-8")
+            if args.div is None:
+                decoded = soup.prettify("utf-8").decode("utf-8")
+            else:
+                element = soup.find("div", class_=args.div)
+                if element is None:
+                    print("Div not found...")
+                    exit(1)
+                decoded = element.prettify("utf-8").decode("utf-8")
             
             with open(f"{args.out}.json", "w", encoding="utf-8") as file:
                 json.dump(decoded, file, indent=4, sort_keys=True)
+
+            with open(f"{args.out}.html", "w", encoding="utf-8") as file:
+                file.write(decoded)
